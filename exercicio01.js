b83712790a9860ac43415ee21c70fefc27fb26e7
Manassés Eugênio de Santana <manasses.dev@gmail.com>
2022-11-28 03:49:29 -0300

    melhoria do regex da atividade 1

--- exercicio01.js
+++ exercicio01.js
@@ -4,8 +4,16 @@ Extraia as colunas da tabela e armazene em uma variável chamada "columns".
 Manipule a variável "columns", separando cada coluna com seu respectivo tipo, em uma string separada.
 */
 
-let script = 'create table author (id number, name string, age number, city string, state string, country string)';
-let regCreateTable = /^create\stable\s(\w+)\s\((.+)\)/;
+let script = `
+    create table author (
+        id number,
+        name string,
+        age number,
+        city string,
+        state string, 
+        country string
+    )`;
+let regCreateTable = /^\s*create\s+table\s+(\w+)\s+\(([\w\s,]+)\)/;
 let result = regCreateTable.exec(script);
 let tableName = result[1];
 let columns = result[2].split(',');
