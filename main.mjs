84b4ca5e282f4e807c75bb8d20066020403699cf
Manassés Eugênio de Santana <manasses.dev@gmail.com>
2023-01-11 11:51:38 -0300

    Update main.mjs
    exercicio 12

--- main.mjs
+++ main.mjs
@@ -1,15 +1,17 @@
 import { Database } from "./database.mjs";
 
-let database = new Database();
-database.execute("create table author (id number, name string, age number, city string, state string, country string)");
-Promise.all([
-    database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)"),
-    database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)"),
-    database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)"),
-]).then(() => {
-    database.execute("select name, age from author").then((result) => {
-        console.log(result)
-    });
-}).catch((e) => {
-    console.log(e);
-})
+(async function() {
+    try {
+        const database = new Database();
+        await database.execute("create table author (id number, name string, age number, city string, state string, country string)");
+        await Promise.all([
+            database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)"),
+            database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)"),
+            database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)"),
+        ]);
+        const result = await database.execute("select name, age from author");
+        console.log(result);
+    } catch (e){
+        console.log(e);
+    }
+})();
