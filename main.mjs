178e8524ad2230da53cea58e68c8c861c538a2d3
Manassés Eugênio de Santana <manasses.dev@gmail.com>
2023-01-11 11:44:40 -0300

    Update main.mjs
    atividade 11

--- main.mjs
+++ main.mjs
@@ -1,13 +1,15 @@
 import { Database } from "./database.mjs";
 
-try {
-    let database = new Database();
-    database.execute("create table author (id number, name string, age number, city string, state string, country string)");
-    database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)");
-    database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)");
-    database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)");
-    database.execute("delete from author where id = 2");
-    console.log(JSON.stringify(database.execute("select name, age from author"), undefined, "   "));
-} catch (error) {
-    console.log(error.message);
-}
+let database = new Database();
+database.execute("create table author (id number, name string, age number, city string, state string, country string)");
+Promise.all([
+    database.execute("insert into author (id, name, age) values (1, Douglas Crockford, 62)"),
+    database.execute("insert into author (id, name, age) values (2, Linus Torvalds, 47)"),
+    database.execute("insert into author (id, name, age) values (3, Martin Fowler, 54)"),
+]).then(() => {
+    database.execute("select name, age from author").then((result) => {
+        console.log(result)
+    });
+}).catch((e) => {
+    console.log(e);
+})
